7fcdf7a9bf52ac2825e5232a063fae14ffe722ab
Justin B <justinleighb@gmail.com>
2020-05-30 03:29:34 -0400

    Updated Main Activity Path and connected recycler view and adapter Added some dummy shopping items just to see how the app looks like

--- app/src/main/java/behrman/justin/shoppinglist/activities/MainActivity.java
+++ app/src/main/java/behrman/justin/shoppinglist/activities/MainActivity.java
@@ -1,8 +1,18 @@
-package behrman.justin.shoppinglist;
+package behrman.justin.shoppinglist.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.Arrays;
+import java.util.List;
+
+import behrman.justin.shoppinglist.R;
+import behrman.justin.shoppinglist.adapters.ShoppingListAdapter;
+import behrman.justin.shoppinglist.model.Category;
+import behrman.justin.shoppinglist.model.ShoppingItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<ShoppingItem> items = Arrays.asList(
+                new ShoppingItem("Good item", "Good description", 6.90, Category.BOOK, true),
+                new ShoppingItem("Good item", "Good description", 6.90, Category.BOOK, true),
+                new ShoppingItem("Good item", "Good description", 6.90, Category.BOOK, true),
+                new ShoppingItem("Good item", "Good description", 6.90, Category.BOOK, true),
+                new ShoppingItem("Good item", "Good description", 6.90, Category.BOOK, true),
+                new ShoppingItem("Good item", "Good description", 6.90, Category.BOOK, true),
+                new ShoppingItem("Good item", "Good description", 6.90, Category.BOOK, true),
+                new ShoppingItem("Good item", "Good description", 6.90, Category.BOOK, true),
+                new ShoppingItem("Bad item", "Bad description", 6.13, Category.ELECTRONIC, false)
+        );
+        RecyclerView recyclerView = findViewById(R.id.list_recycler);
+        recyclerView.setAdapter(new ShoppingListAdapter(items));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
 }
