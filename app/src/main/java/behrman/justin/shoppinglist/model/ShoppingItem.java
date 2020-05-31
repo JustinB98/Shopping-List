fa9f5b32333955061ba494f998e8008ad674d5b5
Justin B <justinleighb@gmail.com>
2020-05-31 19:48:43 -0400

    Added JSON related methods for the ShoppingItem One to go from a ShoppingItem to a JSONObject and from JSONObject to ShoppingItem

--- app/src/main/java/behrman/justin/shoppinglist/model/ShoppingItem.java
+++ app/src/main/java/behrman/justin/shoppinglist/model/ShoppingItem.java
@@ -1,15 +1,24 @@
 package behrman.justin.shoppinglist.model;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Objects;
 
 public class ShoppingItem {
 
+    private static final String TAG = ShoppingItem.class.getSimpleName();
+
     private String name, description;
     private double estimatedPrice;
     private Category category;
     private boolean purchased;
+
+    public ShoppingItem() {}
 
     public ShoppingItem(String name, String description, double estimatedPrice, Category category, boolean purchased) {
         this.name = name;
@@ -65,6 +74,34 @@ public class ShoppingItem {
         this.purchased = item.purchased;
         this.description = item.description;
         this.estimatedPrice = item.estimatedPrice;
+    }
+
+    public JSONObject toJSONObject() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("name", name);
+            jsonObject.put("category", category);
+            jsonObject.put("purchased", purchased);
+            jsonObject.put("description", description);
+            jsonObject.put("estimatedPrice", estimatedPrice);
+        } catch (JSONException e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+        return jsonObject;
+    }
+
+    public static ShoppingItem fromJSONObject(JSONObject jsonObject) {
+        try {
+            String name = jsonObject.getString("name");
+            Category category = Category.valueOf(jsonObject.getString("category"));
+            boolean purchased = jsonObject.getBoolean("purchased");
+            String description = jsonObject.getString("description");
+            double estimatedPrice = jsonObject.getDouble("estimatedPrice");
+            return new ShoppingItem(name, description, estimatedPrice, category, purchased);
+        } catch (JSONException e) {
+            Log.e(TAG, e.getMessage(), e);
+            return new ShoppingItem();
+        }
     }
 
     @Override
