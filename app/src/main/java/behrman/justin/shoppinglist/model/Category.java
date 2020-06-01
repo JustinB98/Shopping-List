dc804b77fc40c3cc48b87fb290df947009980316
Justin B <justinleighb@gmail.com>
2020-05-31 21:49:32 -0400

    Added a image resource id field to all Category objects

--- app/src/main/java/behrman/justin/shoppinglist/model/Category.java
+++ app/src/main/java/behrman/justin/shoppinglist/model/Category.java
@@ -1,5 +1,26 @@
 package behrman.justin.shoppinglist.model;
 
+import behrman.justin.shoppinglist.R;
+
 public enum Category {
-    FOOD, ELECTRONIC, BOOK
+    FOOD(R.drawable.baseline_local_dining_24),
+    ELECTRONIC(R.drawable.baseline_devices_24),
+    BOOK(R.drawable.baseline_menu_book_24);
+
+    private int imageResId;
+
+    Category(int imageResId) {
+        this.imageResId = imageResId;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    @Override
+    public String toString() {
+        String name = this.name();
+        return name.charAt(0) + name.substring(1).toLowerCase();
+    }
+
 }
