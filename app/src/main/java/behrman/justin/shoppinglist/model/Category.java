da6d8e0420d5dae17469529ff21342b22e4e6edf
Justin B <justinleighb@gmail.com>
2020-06-03 10:34:41 -0400

    Added 'Other' option to the list of categories

--- app/src/main/java/behrman/justin/shoppinglist/model/Category.java
+++ app/src/main/java/behrman/justin/shoppinglist/model/Category.java
@@ -5,7 +5,8 @@ import behrman.justin.shoppinglist.R;
 public enum Category {
     FOOD(R.drawable.baseline_local_dining_24),
     ELECTRONIC(R.drawable.baseline_devices_24),
-    BOOK(R.drawable.baseline_menu_book_24);
+    BOOK(R.drawable.baseline_menu_book_24),
+    OTHER(R.drawable.baseline_shopping_bag_24);
 
     private int imageResId;
 
